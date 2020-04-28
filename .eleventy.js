5dd2aed74daf366f7064616ed97ea3ca63c31b3e
5t3ph <thinkdobecreate@gmail.com>
2020-04-28 15:39:00 -0500

    Project structure improvements
    - fix slugs
    - move input items to /src/
    - change output to /public/
    - update .gitignore

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,21 @@
+const slugify = require("slugify");
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("./src/css");
+
+  eleventyConfig.addFilter("slug", function (str) {
+    return slugify(str, {
+      lower: true,
+      replacement: "-",
+      remove: /[*+~.·,()'"`´%!?¿:@\/]/g,
+    });
+  });
+
   return {
     passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      output: "public",
+    },
   };
 };
