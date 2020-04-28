24855bf59eb5bda5f4962b92e2f5428e20cdcd9b
5t3ph <thinkdobecreate@gmail.com>
2020-04-28 17:24:09 -0500

    Swap to published Netlify API

--- src/_data/devposts.js
+++ src/_data/devposts.js
@@ -1,15 +1,9 @@
 const axios = require("axios");
 
 module.exports = async () => {
-  const env = process.env.ELEVENTY_ENV;
+  const apiPosts = await axios.get(
+    "https://moderncss-dot-dev.netlify.app/.netlify/functions/devto",
+  );
 
-  let data;
-
-  if (env === "prod") {
-    data = await axios.get("https://moderncss-dot-dev.netlify.app/.netlify/functions/devto");
-  } else {
-    data = require("./postdata.json");
-  }
-
-  return data;
+  return apiPosts.data;
 };
