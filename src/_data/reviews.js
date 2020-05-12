6087ea18ea221a09227875d68feeef44299af72d
5t3ph <thinkdobecreate@gmail.com>
2020-05-12 13:11:01 -0500

    Reviews and heading anchors

---
+++ src/_data/reviews.js
@@ -0,0 +1,22 @@
+module.exports = () => {
+  return (reviews = [
+    {
+      name: "Chris Coyier",
+      avatar: "https://pbs.twimg.com/profile_images/883049860332609537/IsUISvAq_400x400.jpg",
+      source: "https://css-tricks.com/modern-css-solutions-for-old-css-problems/",
+      quote: "This is a hell of a series by Stephanie Eckles.",
+    },
+    {
+      name: "Chris Heilmann",
+      avatar: "https://pbs.twimg.com/profile_images/1666904408/codepo8_400x400.png",
+      source: "https://twitter.com/codepo8/status/1259869074944901127",
+      quote: "This is excellent.",
+    },
+    {
+      name: "Louis Chenais",
+      avatar: "https://pbs.twimg.com/profile_images/977126032774967296/a7DXKZEV_400x400.jpg",
+      source: "https://twitter.com/ChucKN0risK/status/1260114283771330560",
+      quote: "This is the kind of resource that really deserves recognition. Hats off to @5t3ph 👏",
+    },
+  ]);
+};
