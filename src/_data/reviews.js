fbe5b0bd27e1d8a8ba737f138c2df706387a4614
5t3ph <thinkdobecreate@gmail.com>
2020-05-14 11:28:05 -0500

    Add reviews

--- src/_data/reviews.js
+++ src/_data/reviews.js
@@ -13,10 +13,23 @@ module.exports = () => {
       quote: "This is excellent.",
     },
     {
+      name: "Lynn Fisher (@lynnandtonic)",
+      avatar: "https://pbs.twimg.com/profile_images/477556215401025537/zH_q0-_s_400x400.png",
+      source: "https://twitter.com/lynnandtonic/status/1260950561576247296",
+      quote:
+        "This is very cool...Fun to think about how much we used to struggle with some of these and remember how much CSS has&nbsp;grown.",
+    },
+    {
       name: "Louis Chenais",
       avatar: "https://pbs.twimg.com/profile_images/977126032774967296/a7DXKZEV_400x400.jpg",
       source: "https://twitter.com/ChucKN0risK/status/1260114283771330560",
       quote: "This is the kind of resource that really deserves recognition. Hats off to @5t3ph 👏",
+    },
+    {
+      name: "@SingaporeCSS",
+      avatar: "https://pbs.twimg.com/profile_images/1244842172567805953/PwNEkEQJ_400x400.jpg",
+      source: "https://twitter.com/SingaporeCSS/status/1260748569494994950",
+      quote: "y'all should bookmark this and check back regularly 💙",
     },
   ]);
 };
