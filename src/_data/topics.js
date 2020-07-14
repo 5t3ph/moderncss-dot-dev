795628e26dda4d2dc0c4c0c218d6f8dda34e763e
5t3ph <thinkdobecreate@gmail.com>
2020-07-13 23:26:28 -0500

    Update upcoming topics

--- src/_data/topics.js
+++ src/_data/topics.js
@@ -1,8 +1,5 @@
 module.exports = {
-  15: "CSS grid tricks",
-  16: "Modern heros",
-  17: "Checkboxes",
-  18: "Radio Buttons",
-  19: "Form Selects",
-  20: "Inputs & Textareas",
+  19: "Checkboxes",
+  20: "Form Selects",
+  21: "Inputs & Textareas",
 };
