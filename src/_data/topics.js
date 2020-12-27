78021669a49057f855ddf900815063ac1dc00fd6
5t3ph <thinkdobecreate@gmail.com>
2020-12-27 15:52:33 -0600

    Update topics

--- src/_data/topics.js
+++ src/_data/topics.js
@@ -1,6 +1,7 @@
 module.exports = {
-  24: "CSS Selectors",
-  25: "Developing for Imperfect",
-  26: "Modals",
-  27: "Media",
+  24: "CSS Selectors - Part 1",
+  25: "CSS Selectors - Part 2",
+  26: "Developing for Imperfect",
+  27: "Modals",
+  28: "Media",
 };
